cde50374feb27c27cb4fe4c57e80bc05e2c48489
Lupo Montero <lupomontero@gmail.com>
2019-11-13 15:04:31 -0500

    Updates

--- precache-manifest.dfe43d33672b2dbd7bf548ecb0c4828d.js
+++ precache-manifest.dfe43d33672b2dbd7bf548ecb0c4828d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "beab5d52bba1008a8aabf9c86c69e3e4",
+    "revision": "dbacfa226df36dc5252dbd8275d627c1",
     "url": "/index.html"
   },
   {
-    "revision": "f4bf2b191d4665cca7d2",
-    "url": "/static/css/main.ed31f3ef.chunk.css"
+    "revision": "ce2cc563ba86fbe2a479",
+    "url": "/static/css/main.d752a027.chunk.css"
   },
   {
     "revision": "7c776c8758972a162fd2",
     "url": "/static/js/2.48979a92.chunk.js"
   },
   {
-    "revision": "f4bf2b191d4665cca7d2",
-    "url": "/static/js/main.f3c31319.chunk.js"
+    "revision": "ce2cc563ba86fbe2a479",
+    "url": "/static/js/main.9593ffd0.chunk.js"
   },
   {
     "revision": "6e9429287a69fc1a4211",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ca2167f31cb30b86d0677b113579bd7.js"
+  "/precache-manifest.dfe43d33672b2dbd7bf548ecb0c4828d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
