49b32d1184c5e99bc9847e105c2c0321eb2c835d
Lupo Montero <lupomontero@gmail.com>
2019-12-16 12:49:15 -0500

    Updates info about next hangout (jan 2020)

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -8,10 +8,11 @@ const Calendar = () => (
     <h2><Emoji symbol={'💬'} /> Próximo hangout</h2>
     <div>
       <h3>
-        <a href="https://calendar.google.com/calendar/r/week/2019/11/15?eid=NXQ0MjhjOGllaTdzanA2YjdnOHR2dHQ3YmMgbGFib3JhdG9yaWEubGFfdmhyMmowZ2VycTJvaWRmZm0xNGZvM3R1aW9AZw&ctz=America/Lima&sf=true">
-          15 de Noviembre 2019 14:30h (Lima)
+        <a href="https://calendar.google.com/calendar/r/week/2020/1/24?eid=MGc0bHU1ZWQxcGtiNDFma3NzaDlndjdvdmsgbGFib3JhdG9yaWEubGFfdmhyMmowZ2VycTJvaWRmZm0xNGZvM3R1aW9AZw&ctz=America/Lima&sf=true">
+          24 de Enero 2020 14:30h (Lima)
         </a>
       </h3>
+      {/*
       <ul>
         <li>14:00 - 14:10: Bienvenida / Intro (<a href="https://github.com/bouli">@bouli</a> / <a href="https://github.com/lupomontero">@lupomontero</a>)</li>
         <li>14:10 - 14:35: Cómo no ir de A -&gt; B, Grafos (<a href="https://github.com/lizzie136">@lizzie136</a>)</li>
@@ -19,19 +20,18 @@ const Calendar = () => (
         <li>15:00 - 15:25: Experimento de backend en Bootcamp (<a href="https://github.com/merunga">@merunga</a>)</li>
         <li>15:25 - 15:30: Cierre</li>
       </ul>
-      {/*
-        <p>
-          La agenda todavía está por confirmar. En estos momentos estamos
-          aceptando propuestas de <em>charla</em> si alguien se anima a compartir.
-          Para enviar sus propuestas escriban directamente a&nbsp;
-          <a href="https://github.com/bouli">@bouli</a> o&nbsp;
-          <a href="https://github.com/lupomontero">@lupomontero</a> a través del
-          canal&nbsp;
-          <a href="https://laboratoriala.slack.com/messages/C7TE6F4G7/">
-            #equipo-developers en Slack
-          </a>.
-        </p>
       */}
+      <p>
+        La agenda todavía está por confirmar. En estos momentos estamos
+        aceptando propuestas de <em>charla</em> si alguien se anima a compartir.
+        Para enviar sus propuestas escriban directamente a&nbsp;
+        <a href="https://github.com/bouli">@bouli</a> o&nbsp;
+        <a href="https://github.com/lupomontero">@lupomontero</a> a través del
+        canal&nbsp;
+        <a href="https://laboratoriala.slack.com/messages/C7TE6F4G7/">
+          #equipo-developers en Slack
+        </a>.
+      </p>
     </div>
   </section>
 );
